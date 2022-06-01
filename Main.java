4424fbf84f0e08d065b78d2b047d8f992c73b166
Alexandra Frank <swamp.tea.91@gmail.com>
2022-06-01 19:08:34 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        long count = persons.stream()
+                .filter(p -> p.getAge() < 18)
+                .count();
+
+        List<String> list = persons.stream()
+                .filter(p -> p.getSex() == Sex.MAN)
+                .filter(p -> p.getAge() >= 18 && p.getAge() <= 27)
+                .map(p -> p.getFamily())
+                .collect(Collectors.toList());
+
+        List<Person> workingPersons = persons.stream()
+                .filter(p -> p.getEducation() == Education.HIGHER)
+                .filter(p -> p.getAge() >= 18)
+                .filter(p -> p.getSex() == Sex.WOMAN && p.getAge() <= 60 ||
+                        p.getSex() == Sex.MAN && p.getAge() <= 65)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+    }
+}
